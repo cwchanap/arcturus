5d501ecc8ea368eebc776f8a4bb0d5f827e8c7e1
cwchanap <cwchanap@connect.ust.hk>
2026-07-06 04:15:30 -0700

    feat(slots): wire game client with chip sync (HPA-124)

---
+++ src/lib/slots/index.ts
@@ -0,0 +1,8 @@
+export { SlotsGame } from './SlotsGame';
+export { ReelManager } from './ReelManager';
+export { evaluateGrid, evaluateLine, linePayout } from './payoutCalculator';
+export { GameSettingsManager } from './GameSettingsManager';
+export { SlotsUIRenderer } from './SlotsUIRenderer';
+export { initSlotsClient } from './slotsClient';
+export * from './constants';
+export * from './types';
